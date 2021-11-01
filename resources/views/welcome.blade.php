ef3b5d5b26c964325ee428de0135aa579d89d3d8
osajw <morgan.jw@aol.com>
2021-11-01 17:47:24 +0100

    icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" sizes="180x180" href="./img/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./img/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./img/icons/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="./img/icons/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#ffffff">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
         <meta name="msapplication-navbutton-color" content="#8bc34a">
         <meta name="theme-color" content="#8bc34a">
         <meta name="csrf-token" content="{{ csrf_token() }}">
